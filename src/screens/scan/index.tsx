cb3b40174b9ed6f15108aa17b93688dd1e1f764c
kennedymakokha <katchibo2@gmail.com>
2025-04-14 15:01:54 +0300

    fixing the scaner

--- src/screens/scan/index.tsx
+++ src/screens/scan/index.tsx
@@ -1,48 +1,16 @@
 import React from 'react';
 import { View, Text, StyleSheet, Linking, Alert } from 'react-native';
-import QRCodeScanner from 'react-native-qrcode-scanner';
-import { RNCamera } from 'react-native-camera';
+
 
 const QRScanScreen = () => {
-  const onSuccess = (e: { data: string }) => {
-    Alert.alert("QR Code Scanned", e.data, [
-      {
-        text: "Open Link",
-        onPress: () => Linking.openURL(e.data),
-      },
-      { text: "Cancel", style: "cancel" }
-    ]);
-  };
 
   return (
-    <View style={styles.container}>
-      <QRCodeScanner
-        onRead={onSuccess}
-        flashMode={RNCamera.Constants.FlashMode.auto}
-        showMarker
-        topContent={<Text style={styles.centerText}>Scan a QR Code</Text>}
-        bottomContent={<Text style={styles.bottomText}>Align the QR inside the frame</Text>}
-      />
+    <View >
+
     </View>
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  centerText: {
-    fontSize: 18,
-    padding: 32,
-    color: '#000',
-    textAlign: 'center',
-  },
-  bottomText: {
-    fontSize: 16,
-    padding: 20,
-    color: '#777',
-    textAlign: 'center',
-  },
-});
+
 
 export default QRScanScreen;
---
+++ src/screens/scan/permissionPage.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const PermissionsPage = () => {
+    return (
+        <View>
+            <Text>PermissionsPage</Text>
+        </View>
+    )
+}
+
+export default PermissionsPage
